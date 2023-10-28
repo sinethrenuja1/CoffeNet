a3286c92a1ac6c674c3f4678a97e4ee9bc558daf
sinethrenuja1 <sinethrenuja312@gmail.com>
2023-10-28 13:17:45 +0530

    Project

---
+++ app/src/main/java/com/example/coffee_net/Register.java
@@ -0,0 +1,14 @@
+package com.example.coffee_net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
